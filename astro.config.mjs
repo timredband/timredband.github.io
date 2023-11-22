5fd717a8a9bcaac6beed4b341dcf0659faf50db6
Tim Redband <tim.redband@dunnhumby.com>
2023-11-22 13:35:08 -0500

    add blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://timredband.github.io',
+    integrations: [tailwind(), mdx()],
 });
